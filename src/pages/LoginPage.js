f693383811e6e3ad89383c029c3a36644b3a620b
Manoj Kumar <manojdx44@gmail.com>
2023-07-07 10:33:42 +0530

    First commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../features/auth/components/Login'
+
+function LoginPage() {
+  return (
+    <div>
+      <Login>
+        
+      </Login>
+    </div>
+  )
+}
+
+export default LoginPage
